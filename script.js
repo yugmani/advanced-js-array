cd35c4f9da335c984c9615c411c2f7e621293b0b
yugmani <yugmani@yahoo.com>
2021-05-17 11:52:24 -0700

    Javascript at() method

--- script.js
+++ script.js
@@ -40,8 +40,24 @@ const myArray = Array.from({ length: 10 }, () =>
   Math.floor(Math.random() * 10)
 );
 
-console.log(myArray);
+// console.log(myArray);
 
 // Expected [0, 0, 8, 5, 1, 5, 4, 5, 9, 5] may be different in your case
 // Reference: https://attacomsian.com/blog/javascript-array-from
 // Reference https://www.ma-no.org/en/programming/javascript/how-to-populate-an-array-with-random-numbers-in-javascript
+
+// 5. JavaScript Array at() method
+
+const fruits = ['banana', 'cherry', 'peach', 'berry', 'pineaple'];
+
+const fruitAt3 = fruits[3];
+console.log('Fruit at 3: ' + fruitAt3); // Expected "berry";
+const fruitLast = fruits[fruits.length - 1];
+console.log('last: ' + fruitLast); // Expected "pineaple";
+
+//at(index) Method
+// The at(index) method accepts both positive and negative numbers as an index.
+// The negative index count back from the array whereas, the positive index count from the beginning as usual.
+
+const fruitsAt0 = fruits.at(-2);
+console.log('Fruits at 0: ' + fruitsAt0);
