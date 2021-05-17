3b709eb1b4dbe48db8a4f149ae0a61fd3f6e6bc5
yugmani <yugmani@yahoo.com>
2021-05-16 22:37:31 -0700

    function to flatten nested array (multidimenstional array)

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-// Flatten first level array.
+const array1 = [[1, 2], [3, [4, 5], 6], [7], [8, 9, 10]];
+
+// Flatten single dimenstional array.
 function justFlatten(arr) {
   const a_flat = arr.flat();
   a_flat.forEach(item => console.log(item));
 }
 
-justFlatten([[1, 2], [3, [4, 5], 6], [7], [8, 9, 10]]);
+// justFlatten(array1);
 
-// const array1 = [[1, 2], [3, [4, 5], 6], [7], [8, 9, 10]];
+// Flatten multi-dimenstional array
+function nestedFlatten(arr) {
+  return arr.reduce(
+    (accumulator, currentVal) =>
+      Array.isArray(currentVal)
+        ? accumulator.concat(nestedFlatten(currentVal))
+        : accumulator.concat(currentVal),
+    []
+  );
+}
+
+console.log(nestedFlatten(array1));
